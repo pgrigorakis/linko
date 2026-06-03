acf702e15d488678f187195c9f0f4e669ee64611
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-06-03 23:41:48 +0200

    Added function to return a logger. Write location based on env value from CLI.

--- logger.go
+++ logger.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func requestLogger(logger *log.Logger) func(http.Handler) http.Handler {
@@ -12,4 +15,18 @@ func requestLogger(logger *log.Logger) func(http.Handler) http.Handler {
 			logger.Printf("Served request: %s %s", r.Method, r.URL.Path)
 		})
 	}
+}
+
+func initialiseLogger() (*log.Logger, error) {
+	fileName := os.Getenv("LINKO_LOG_FILE")
+	if fileName != "" {
+		f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			return nil, fmt.Errorf("could not open file: %v", err)
+		}
+		multiWriter := io.MultiWriter(os.Stderr, f)
+		return log.New(multiWriter, "", log.LstdFlags), nil
+	}
+
+	return log.New(os.Stderr, "", log.LstdFlags), nil
 }
