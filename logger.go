aa8c54d20bf465015c64bfd6a009d8b884ba6477
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-06-04 18:34:06 +0200

    Added stack tracing and group with error and stack trace to replaceAttr function

--- logger.go
+++ logger.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
+
+	pkgerr "github.com/pkg/errors"
 )
 
 type closeFunc func() error
+
+type stackTracer interface {
+	error
+	StackTrace() pkgerr.StackTrace
+}
 
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
@@ -24,7 +32,9 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 
 func initialiseLogger(logFile string) (*slog.Logger, closeFunc, error) {
 	handlers := []slog.Handler{
-		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			Level:       slog.LevelDebug,
+			ReplaceAttr: replaceAttr}),
 	}
 	closer := func() error { return nil }
 
@@ -36,7 +46,8 @@ func initialiseLogger(logFile string) (*slog.Logger, closeFunc, error) {
 		bufferedFile := bufio.NewWriterSize(f, 8192)
 
 		infoHandler := slog.NewJSONHandler(bufferedFile, &slog.HandlerOptions{
-			Level: slog.LevelInfo,
+			Level:       slog.LevelInfo,
+			ReplaceAttr: replaceAttr,
 		})
 		handlers = append(handlers, infoHandler)
 
@@ -52,4 +63,24 @@ func initialiseLogger(logFile string) (*slog.Logger, closeFunc, error) {
 
 	logger := slog.New(slog.NewMultiHandler(handlers...))
 	return logger, closer, nil
+}
+
+func replaceAttr(groups []string, a slog.Attr) slog.Attr {
+	if a.Key == "error" {
+		err, ok := a.Value.Any().(error)
+		if !ok {
+			return a
+		}
+		if stackErr, ok := errors.AsType[stackTracer](err); ok {
+			return slog.GroupAttrs("error", slog.Attr{
+				Key:   "message",
+				Value: slog.StringValue(stackErr.Error()),
+			}, slog.Attr{
+				Key:   "stack_trace",
+				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
+			})
+		}
+		return a
+	}
+	return a
 }
