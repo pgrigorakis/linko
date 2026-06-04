18fdeed9338da7840bc9198a4339ccdbd6aad04c
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-06-04 12:25:44 +0200

    Created info and debug loggers using log/slog

--- logger.go
+++ logger.go
@@ -3,33 +3,41 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
-	"log"
+	"log/slog"
 	"net/http"
 	"os"
 )
 
 type closeFunc func() error
 
-func requestLogger(logger *log.Logger) func(http.Handler) http.Handler {
+func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			next.ServeHTTP(w, r)
-			logger.Printf("Served request: %s %s", r.Method, r.URL.Path)
+			logger.Info(fmt.Sprintf("Served request: %s %s", r.Method, r.URL.Path))
 		})
 	}
 }
 
-func initialiseLogger(logFile string) (*log.Logger, closeFunc, error) {
+func initialiseLogger(logFile string) (*slog.Logger, closeFunc, error) {
+	handlers := []slog.Handler{
+		slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug}),
+	}
+	closer := func() error { return nil }
+
 	if logFile != "" {
 		f, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 		if err != nil {
 			return nil, nil, fmt.Errorf("could not open file: %v", err)
 		}
 		bufferedFile := bufio.NewWriterSize(f, 8192)
-		multiWriter := io.MultiWriter(os.Stderr, bufferedFile)
 
-		close := func() error {
+		infoHandler := slog.NewTextHandler(bufferedFile, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		})
+		handlers = append(handlers, infoHandler)
+
+		closer = func() error {
 			err := bufferedFile.Flush()
 			if err != nil {
 				return err
@@ -37,13 +45,8 @@ func initialiseLogger(logFile string) (*log.Logger, closeFunc, error) {
 			err = f.Close()
 			return err
 		}
-
-		return log.New(multiWriter, "", log.LstdFlags), close, nil
 	}
 
-	close := func() error {
-		return nil
-	}
-
-	return log.New(os.Stderr, "", log.LstdFlags), close, nil
+	logger := slog.New(slog.NewMultiHandler(handlers...))
+	return logger, closer, nil
 }
