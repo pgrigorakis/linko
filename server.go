641e3e73d2499da69aaeff456d1920bbc7689f3b
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-06-09 23:56:23 +0200

    Added paths for profiling

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
+	"net/http/pprof"
 	"os"
 
 	"boot.dev/linko/internal/store"
@@ -36,6 +37,9 @@ func newServer(store store.Store, port int, cancel context.CancelFunc, logger *s
 	}
 
 	mux.HandleFunc("GET /", s.handlerIndex)
+	mux.Handle("GET /debug/pprof/", s.authMiddleware(http.HandlerFunc(pprof.Index)))
+	mux.Handle("GET /debug/pprof/profile", s.authMiddleware(http.HandlerFunc(pprof.Profile)))
+	mux.Handle("GET /debug/pprof/heap", s.authMiddleware(pprof.Handler("heap")))
 	mux.Handle("POST /api/login", s.authMiddleware(http.HandlerFunc(s.handlerLogin)))
 	mux.Handle("POST /api/shorten", s.authMiddleware(http.HandlerFunc(s.handlerShortenLink)))
 	mux.Handle("GET /api/stats", s.authMiddleware(http.HandlerFunc(s.handlerStats)))
